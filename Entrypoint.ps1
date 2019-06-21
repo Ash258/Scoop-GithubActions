211f6ae892cdc577e0eb8e0ba9d95303bfbeb416
Jakub Čábera <cabera.jakub@gmail.com>
2019-06-21 21:24:41 +0200

    TODO

--- Entrypoint.ps1
+++ Entrypoint.ps1
@@ -5,7 +5,9 @@ param (
 )
 
 function Invoke-GithubRequest {
-
+	param([Object] $Body, [String] $query)
+	# TODO:
+	return Invoke-WebRequest -Headers $HEADER -Body (ConvertTo-Json $Body -Depth 8 -Compress) -Method Post "$URI/repos/Ash258/GithubActionsBucketForTesting/issues/5/comments"
 }
 
 function Add-Comment {
@@ -17,14 +19,14 @@ function Add-Comment {
 }
 
 function Write-Log {
+	[Parameter(Mandatory, ValueFromRemainingArguments)]
 	param ([String[]] $Message)
 	Write-Output "LOG: $($Message -join "`r`n    ")"
 }
 
-$URI='https://api.github.com'
-$API_VERSION='v3'
-$API_HEADER="Accept: application/vnd.github.$API_VERSION+json; application/vnd.github.antiope-preview+json"
-
+$URI = 'https://api.github.com'
+$API_VERSION = 'v3'
+$API_HEADER = "Accept: application/vnd.github.$API_VERSION+json; application/vnd.github.antiope-preview+json"
 $HEADER = @{
 	'Authorization' = "token $env:GITHUB_TOKEN"
 }
@@ -33,7 +35,7 @@ $EVENT = Get-Content $env:GITHUB_EVENT_PATH -Raw | ConvertFrom-Json
 
 if ($Type -eq 'Issue') {
 	if ($EVENT.action -ne 'opened') {
-		Write-Log 'Every issues action except ''opened'' is ignored.'
+		Write-Log 'Every issues action except ''opened'' are ignored.'
 		exit 0
 	}
 	$envs = [Environment]::GetEnvironmentVariables().Keys
