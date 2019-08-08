e4faebc6fab878ea4e71c9a28cfd6675996561f8
Ash258 <cabera.jakub@gmail.com>
2019-08-08 18:28:02 +0200

    Tweaks

--- Entrypoint.ps1
+++ Entrypoint.ps1
@@ -547,17 +547,15 @@ function Initialize-Issue {
 #endregion Function pool
 
 #region Main
-# For dot sourcing whole file inside tests
-if ($env:TESTS) { return }
-
 $VerbosePreference = 'Continue'
 Test-NestedBucket
 Initialize-NeededSettings
 
+Write-Log 'Importing all modules'
 # Load all scoop's modules.
 # Dot sourcing needs to be done on highest scope possible to propagate into lower scopes
-Write-Log 'Importing all modules'
 Get-ChildItem (Join-Path $env:SCOOP_HOME 'lib') '*.ps1' | ForEach-Object { . $_.FullName }
+# Same for function recreating. Needs to be done after import and on highest scope
 Initialize-MockedFunctionsFromCore
 
 Write-Log 'FULL EVENT' $EVENT_RAW
