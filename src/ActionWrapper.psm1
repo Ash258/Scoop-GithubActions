be26c8a80af605adc97f398925574ffdb8ec1bd1
Ash258 <cabera.jakub@gmail.com>
2019-08-08 18:20:40 +0200

    Simplify and spaces

--- src/ActionWrapper.psm1
+++ src/ActionWrapper.psm1
@@ -1,14 +1,14 @@
 Import-Module (Join-Path $PSScriptRoot '..\Variables.psm1')
-Get-ChildItem $PSScriptRoot 'Action\*' -File | Select-Object -ExpandProperty Fullname | Import-Module
+Get-ChildItem $PSScriptRoot 'Action\*.psm1' -File | Select-Object -ExpandProperty Fullname | Import-Module
 
 function Invoke-Action {
-	switch ($EVENT_TYPE) {
-		'issues' { Initialize-Issue }
-		'pull_request' { Initialize-PR }
-		'issue_comment' { Initialize-PR }
-		'schedule' { Initialize-Scheduled }
-		default { Write-Log 'Not supported event type' }
-	}
+    switch ($EVENT_TYPE) {
+        'issues' { Initialize-Issue }
+        'pull_request' { Initialize-PR }
+        'issue_comment' { Initialize-PR }
+        'schedule' { Initialize-Scheduled }
+        default { Write-Log 'Not supported event type' }
+    }
 }
 
 Export-ModuleMember -Function Invoke-Action
