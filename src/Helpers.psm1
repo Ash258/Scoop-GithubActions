9614fea28341199a04d217d04f2fa0f054b2e978
Jakub Čábera <cabera.jakub@gmail.com>
2019-08-21 10:23:05 +0200

    Test

--- src/Helpers.psm1
+++ src/Helpers.psm1
@@ -92,16 +92,14 @@ function Initialize-NeededSettings {
         Initialize all settings, environment, configurations to work as expected.
     #>
     @('buckets', 'cache') | ForEach-Object { New-Item "$env:SCOOP/$_" -Force -ItemType Directory | Out-Null }
+
     if ($env:GITH_EMAIL) {
         git config --global user.email $env:GITH_EMAIL
     } else {
         Write-Log 'Pushing is not possible without email environment'
     }
-
     git config --global user.name ($env:GITHUB_REPOSITORY -split '/')[0]
     git config --global credential.helper store
-
-    Add-Content "$HOME\.git-credentials" "https://${env:GITHUB_TOKEN}:x-oauth-basic@github.com`n"
 
     # Log all environment variables
     Write-Log 'Environment' (Get-EnvironmentVariables)
