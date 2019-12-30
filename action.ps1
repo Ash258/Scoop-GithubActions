de1601d8ff056adcbfb99ccb34d838a0f81029d8
Jakub Čábera <cabera.jakub@gmail.com>
2019-12-30 03:12:11 +0100

    test checkout

--- action.ps1
+++ action.ps1
@@ -11,6 +11,7 @@ $VerbosePreference = 'Continue' # Preserve verbose in logs
 Test-NestedBucket
 Initialize-NeededSettings
 
+git checkout -B 'COSI'
 Write-Log 'Importing all modules'
 # Load all scoop's modules.
 # Dot sourcing needs to be done on highest scope possible to propagate into lower scopes
