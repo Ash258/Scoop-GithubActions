eecc9fd84ed475fa8db1bd4be461d0bd926246f6
Jakub Čábera <cabera.jakub@gmail.com>
2019-12-31 15:36:30 +0100

    test

--- action.ps1
+++ action.ps1
@@ -11,7 +11,8 @@ $VerbosePreference = 'Continue' # Preserve verbose in logs
 Test-NestedBucket
 Initialize-NeededSettings
 
-git branch -vv
+git config
+git config --get user.email
 Write-Log 'Importing all modules'
 # Load all scoop's modules.
 # Dot sourcing needs to be done on highest scope possible to propagate into lower scopes
